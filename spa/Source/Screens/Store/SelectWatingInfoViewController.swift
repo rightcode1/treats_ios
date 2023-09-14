1b8af47aeb09dc0cd817f44ad2fd1abda244e69f
namki1222 <53803965+namki1222@users.noreply.github.com>
2023-09-14 17:05:16 +0900

    20230914 깃 관리 목적 백업 완

--- spa/Source/Screens/Store/SelectWatingInfoViewController.swift
+++ spa/Source/Screens/Store/SelectWatingInfoViewController.swift
@@ -28,7 +28,7 @@ class SelectWatingInfoViewController: BaseViewController {
   var selectedBedCount = 1
   var selectedTime = Date()
 
-  var timeList = [Date]()
+  var timeList = [ReservationTime]()
   var selectDelegate: selectWaitProtocol?
 
   override func viewDidLoad() {
@@ -40,7 +40,7 @@ class SelectWatingInfoViewController: BaseViewController {
       guard let self = self, let startDate = visibleDates.monthDates.first?.date else { return }
       self.currentMonthLabel.text = "\(startDate.year). \(startDate.month)"
     }
-    generateTimeList()
+    getSchedule()
 
     applyButton.rx.tap
       .bind(onNext: { [weak self] in
@@ -68,29 +68,88 @@ class SelectWatingInfoViewController: BaseViewController {
       })
       .disposed(by: disposeBag)
   }
+//
+//  func generateTimeList() {
+//    timeList = []
+//
+//    var startDate = DateComponents(
+//      calendar: Calendar.current,
+//      year: selectedDate.year,
+//      month: selectedDate.month,
+//      day: selectedDate.day,
+//      hour: 0,
+//      minute: 0,
+//      second: 0
+//    ).date!// ?? Date()
+//
+//    while startDate.day == selectedDate.day {
+//      timeList.append(startDate)
+//      startDate.addTimeInterval(60*30)
+//    }
+//
+//    timeList = timeList.filter({ $0 > Date() })
+//    timeList.sort(by: { $0 < $1 })
+//    if let firstTime = timeList.first {
+//      self.selectedTime = firstTime
+//    }
+//    timeCollectionView.reloadData()
+//  }
+//
+  func getSchedule() {
+    APIService.shared.storeAPI.rx.request(.getStoreSchedule(storeId: storeId, date: selectedDate.yyyyMMdd))
+      .filterSuccessfulStatusCodes()
+      .map(GetScheduleResponse.self)
+      .subscribe(onSuccess: { response in
+        self.timeList = []
+        
+        let calendar = Calendar(identifier: .gregorian)
+        let currentDate = Date() // 현재 로컬 시간을 가져옵니다.
 
-  func generateTimeList() {
-    timeList = []
-    var startDate = DateComponents(
-      calendar: Calendar.current,
-      year: selectedDate.year,
-      month: selectedDate.month,
-      day: selectedDate.day,
-      hour: 0,
-      minute: 0,
-      second: 0
-    ).date!// ?? Date()
+        var date = calendar.date(from: DateComponents(year: self.selectedDate.year, month: self.selectedDate.month, day: self.selectedDate.day, hour: 0, minute: 0))!
+        let endDate = calendar.date(byAdding: .day, value: 1, to: date)!
 
-    while startDate.day == selectedDate.day {
-      timeList.append(startDate)
-      startDate.addTimeInterval(60*30)
-    }
-    timeList = timeList.filter({ $0 > Date() })
-    timeList.sort(by: { $0 < $1 })
-    if let firstTime = timeList.first {
-      self.selectedTime = firstTime
-    }
-    timeCollectionView.reloadData()
+        var dateList = [Date]()
+        while (date < endDate) {
+            date.addTimeInterval(60 * 30)
+            
+            // 시작 날짜가 현재 로컬 시간 이후인 경우에만 추가
+            if date >= currentDate {
+                dateList.append(date)
+            }
+        }
+        print(dateList)
+        print("!!")
+        print(self.selectedTime)
+        self.timeList = dateList.map({ ReservationTime(date: $0, bedCount: 0) })
+        self.timeList = self.timeList.filter({$0.date >= self.selectedTime})
+        print(self.timeList)
+        
+        response.data.forEach { bed in
+          (bed.schedules ?? []).forEach { schedule in
+            let date = Date.dateFromISO8601String(schedule.date)!
+            if let index = self.timeList.firstIndex(where: { $0.date.isSameTime(date) }) {
+              self.timeList[index].bedCount += 1
+            }
+          }
+        }
+        //        self.timeList = self.timeList.filter({ $0.date > Date() })
+        self.timeList.enumerated().forEach({ (index, item) in
+          if item.date < Date() {
+            self.timeList[index].bedCount = 0
+          }
+        })
+        self.timeList.sort(by: { $0.date < $1.date})
+        self.timeList = self.timeList.filter{$0.bedCount >= self.selectedBedCount}
+        self.timeCollectionView.reloadData()
+        
+        if let time = self.timeList.filter({ $0.date > Date() }).first {
+          if let index = self.timeList.firstIndex(where: { $0.date == time.date }) {
+            self.timeCollectionView.scrollToItem(at: IndexPath(row: index, section: 0), at: .left, animated: false)
+          }
+        }
+      }, onFailure: { error in
+      })
+      .disposed(by: disposeBag)
   }
 }
 
@@ -130,7 +189,7 @@ extension SelectWatingInfoViewController: JTACMonthViewDataSource, JTACMonthView
   func calendar(_ calendar: JTACMonthView, didSelectDate date: Date, cell: JTACDayCell?, cellState: CellState, indexPath: IndexPath) {
     selectedDate = date
     calendarView.reloadData()
-    generateTimeList()
+    getSchedule()
   }
 
   func calendar(_ calendar: JTACMonthView, didScrollToDateSegmentWith visibleDates: DateSegmentInfo) {
@@ -162,7 +221,7 @@ extension SelectWatingInfoViewController: UICollectionViewDataSource, UICollecti
       return cell
     } else {
       let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
-      let time = timeList[indexPath.item]
+      let time = timeList[indexPath.item].date
       (cell.viewWithTag(1) as! UILabel).text = time.ahmm
       if time.hour == selectedTime.hour && time.minute == selectedTime.minute {
         cell.borderColor = .black
@@ -178,7 +237,7 @@ extension SelectWatingInfoViewController: UICollectionViewDataSource, UICollecti
     if collectionView == bedCountCollectionView {
       selectedBedCount = indexPath.item+1
     } else {
-      selectedTime = timeList[indexPath.item]
+      selectedTime = timeList[indexPath.item].date
     }
 
     collectionView.reloadData()
